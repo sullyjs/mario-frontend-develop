3f3b1b18423cdf31a57053825b3da10352ac1fb6
TrasherTravis <trashertravis@gmail.com>
2021-03-18 00:16:59 +0530

    Update UnlockButton.tsx

--- src/components/UnlockButton.tsx
+++ src/components/UnlockButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button, useWalletModal } from '@marioswap-libs/uikit'
 import useAuth from 'hooks/useAuth'
 import useI18n from 'hooks/useI18n'
@@ -6,17 +6,22 @@ import Countdown from "react-countdown";
 
 
 const UnlockButton = (props) => {
+  const [completion, setCompletion] = useState(false);
   const Completionist = () => <span>{TranslateString(292, 'Unlock Wallet')}</span>;
-
+  const renderer = ({ formatted:{hours} , minutes, seconds, completed }) => {
+    if (completed)
+    {
+      setCompletion(true)
+      }
+    return completed?(<Completionist />):(<span>{`${hours}h `}:{` ${minutes}m `}:{` ${seconds}s`}</span>)
+};
   const TranslateString = useI18n()
   const { login, logout } = useAuth()
   const { onPresentConnectModal } = useWalletModal(login, logout)
 
   return (
-    <Button onClick={onPresentConnectModal} {...props}>
-      <Countdown daysInHours date={Date.parse('2021-03-20T00:00:00.000+05:30')}>
-      <Completionist />
-    </Countdown>
+    <Button disabled={!completion} onClick={onPresentConnectModal} {...props}>
+      <Countdown daysInHours renderer={renderer} date={Date.parse('2021-03-20T00:00:00.000+05:30')} />
     </Button>
   )
 }
